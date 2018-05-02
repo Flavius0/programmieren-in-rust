eebf4934568baf46e92aadb6034cb9901d2a10be
Fabian Brosda <f.brosda@gmx.de>
2018-05-02 21:55:01 +0200

    sheet04
    commit a91a1b6dde31a78e8fe3220fe606d2663816e9d3
    Author: Fabian Brosda <f.brosda@gmx.de>
    Date:   Wed May 2 21:54:41 2018 +0200
    
        some cleanup and comments sheet04/task2
    
    commit 82556d196f375d00c6574dbb31c32261aeb9acd0
    Author: Fabian Brosda <f.brosda@gmx.de>
    Date:   Wed May 2 21:21:59 2018 +0200
    
        error handling sheet04/task2
    
    commit 977282dcb61668953d395edc00a1a0cbfa16b4d3
    Author: Fabian Brosda <f.brosda@gmx.de>
    Date:   Wed May 2 00:09:08 2018 +0200
    
        working calculator (without errorhandling) sheet04/task2
    
    commit 2a13f3a044db1f769f94d770403a21a85694d25b
    Author: Fabian Brosda <f.brosda@gmx.de>
    Date:   Tue May 1 17:05:51 2018 +0200
    
        tokenization + expression evaluation sheet04/task2
    
    commit 04124765240978bf08776ad317e532e1c9c5a6fa
    Author: Fabian Brosda <f.brosda@gmx.de>
    Date:   Mon Apr 30 18:46:52 2018 +0200
    
        improved is happy prime sheet04/task1

--- aufgaben/sheet04/task1/bad.rs
+++ aufgaben/sheet04/task1/bad.rs
@@ -1,69 +1,40 @@
-// print if they are both
-fn main() -> () {
-    for iterationnumber in &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] {
-        let iterationnumber = *iterationnumber;
-        if !happy_prime(iterationnumber) {} else {
-            print!("{}", iterationnumber);
-            println!(" is a happy prime!");
+/// print all happy primes between 1 and 20
+fn main() {
+    for i in 1..21 {
+        if is_happy(i) && is_prime(i) {
+            println!("{} is a happy prime!", i);
         }
     }
-
 }
 
-// is it botH?
-fn happy_prime(n: i32) -> bool {
-    match check_if_number_is_happy(n) {
-        false => return false,
-        _ => {}
+/// check if given i32 is a [happy number][1]?
+///
+/// [1]: https://en.wikipedia.org/wiki/Happy_number
+fn is_happy(n: i32) -> bool {
+    fn to_digits(mut n: i32) -> Vec<i32> {
+        let mut res = Vec::new();
+        while n > 0 {
+            res.push(n % 10);
+            n = n / 10;
+        }
+        res
     }
+    match n {
+        1 => true,
+        4 => false,
+        t => is_happy(to_digits(t).iter().map(|v| v*v).sum()),
+    }
+}
 
-    if check_if_number_is_prime(n) == true {
-        return true;
-    } else {
+/// check if given i32 is priem?
+fn is_prime(n: i32) -> bool {
+    if n < 2 {
         return false;
     }
-}
-
-// Is it a happy number? https://en.wikipedia.org/wiki/Happy_number
-fn check_if_number_is_happy(number: i32) -> bool {
-    let mut  number: i32= number;
-
-    while number > 1 {
-        let mut tmp = 0;
-        while number > 0 {
-            tmp = tmp + (number %10) * (number%10);
-            number = number / 10;
-        }
-        number = tmp;
-
-        // We ended up in a cycle -> not happy
-        if (number == 4) {
+    for d in 2..n {
+        if n % d == 0 {
             return false;
         }
     }
-
-    return true;
-}
-
-// is it priem?
-fn check_if_number_is_prime(n: i32) -> bool {
-    if n == 1 {
-        return false;
-    }
-
-    if n == 2 {
-        return false;
-    }
-
-    let mut teilerGefunden:bool = false;
-
-    let mut teiler:i32= 2;
-    while (teiler < n) {
-        if (n % teiler == 0) {
-            teilerGefunden = true;
-        }
-        teiler = teiler + 1;
-    }
-
-    return !teilerGefunden;
+    true
 }
--- aufgaben/sheet04/task2/calculator.rs
+++ aufgaben/sheet04/task2/calculator.rs
@@ -1,3 +1,4 @@
+use std::slice::Iter;
 
 fn main() {
     loop {
@@ -5,13 +6,244 @@ fn main() {
         let input = read_string();
         if input.is_empty() {
             continue;
+        } else {
+            let mut tokens = match Token::tokenize(&input) {
+                Ok(ret) => ret,
+                Err(e) => {
+                    println!("{}", e.description());
+                    continue;
+                }
+            };
+            let expr = match Expr::parse(&mut tokens) {
+                Ok(ret) => ret,
+                Err(e) => {
+                    println!("{}", e.description());
+                    continue;
+                }
+            };
+            println!("{} = {}", input, expr.evaluate());
         }
-
-        // Debug output
-        println!("{}", input);
     }
 }
 
+/// Exceptions, which can occure during tokenization
+#[derive(Clone, Copy, Debug)]
+enum LexError {
+    Overflow,
+    UnexpectedChar(char),
+}
+
+impl LexError {
+    fn description(&self) -> String {
+        match *self {
+            LexError::UnexpectedChar(c) => format!("Unexpected character: {}", c),
+            LexError::Overflow => String::from("Integer overflow"),
+        }
+    }
+}
+
+/// Exceptions, which can occur during parsing
+#[derive(Clone, Copy, Debug)]
+enum ParseError {
+    UnexpectedToken(Token),
+    MissingParen,
+    UnexpectedEol,
+    UnnecessaryToken,
+}
+
+impl ParseError {
+    fn description(&self) -> String {
+        match *self {
+            ParseError::UnexpectedToken(ref t) => format!("Invalid token {:?} found", t),
+            ParseError::MissingParen => String::from("Closing ')' missing"),
+            ParseError::UnexpectedEol => String::from("Unexpected end of input string"),
+            ParseError::UnnecessaryToken => String::from("Unmatched Tokens"),
+        }
+    }
+}
+
+/// Representation of a token
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Token {
+    Plus,
+    Minus,
+    OpenParen,
+    CloseParen,
+    Space,
+    Number(i32),
+}
+
+impl Token {
+    /// Parse the input string and generate tokens
+    fn tokenize(line: &str) -> Result<Vec<Self>, LexError> {
+        /// helper function for numbers (multiple characters)
+        fn push_number(vec: &mut Vec<Token>, num: String) -> Result<String, LexError> {
+            if !num.is_empty() {
+                match num.parse() {
+                    Ok(ret) => vec.push(Token::Number(ret)),
+                    _ => return Err(LexError::Overflow),
+                }
+            }
+            Ok(String::new())
+        }
+
+        let mut ret = Vec::new();
+        let mut input = String::new();
+
+        for c in line.chars() {
+            let token = match c {
+                '+' => Token::Plus,
+                '-' => Token::Minus,
+                '(' => Token::OpenParen,
+                ')' => Token::CloseParen,
+                c if c.is_whitespace() => Token::Space,
+                c @ '0'...'9' => {
+                    input.push(c);
+                    continue;
+                }
+                _ => return Err(LexError::UnexpectedChar(c)),
+            };
+
+            input = match push_number(&mut ret, input) {
+                Ok(ret) => ret,
+                Err(e) => return Err(e),
+            };
+            if Token::Space != token {
+                ret.push(token);
+            }
+        }
+
+        match push_number(&mut ret, input) {
+            Err(e) => Err(e),
+            _ => Ok(ret),
+        }
+    }
+}
+
+/// Representation of an operator
+#[derive(Clone, Copy, Debug)]
+enum Op {
+    Plus,
+    Minus,
+}
+
+impl Op {
+    /// apply operator to two numbers
+    fn apply(&self, left: i32, right: i32) -> i32 {
+        match *self {
+            Op::Plus => left + right,
+            Op::Minus => left - right,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum Operand {
+    Num(i32),
+    Exp(Box<Expr>),
+}
+
+impl Operand {
+    fn get(&self) -> i32 {
+        match self {
+            &Operand::Num(num) => num,
+            &Operand::Exp(ref expr) => (*expr).evaluate(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum Expr {
+    Single(Operand),
+    Compound {
+        op: Op,
+        left: Operand,
+        right: Operand,
+    },
+}
+
+impl Expr {
+    fn evaluate(&self) -> i32 {
+        match *self {
+            Expr::Single(ref v) => v.get(),
+            Expr::Compound {
+                ref op,
+                ref left,
+                ref right,
+            } => op.apply(left.get(), right.get()),
+        }
+    }
+
+    /// convert the token string to an parsetree
+    fn parse(input: &[Token]) -> Result<Expr, ParseError> {
+        /// parse a possible expression
+        ///```
+        ///expr    := ⟨operand⟩ [⟨op⟩ ⟨operand⟩]
+        ///```
+        fn parse_expr<'a>(iter: &mut Iter<'a, Token>) -> Result<Expr, ParseError> {
+            let lhs = match parse_operand(iter) {
+                Ok(ret) => ret,
+                Err(e) => return Err(e),
+            };
+
+            // early return if only left hand side exists
+            match iter.as_slice().first() {
+                Some(&Token::CloseParen) | None => return Ok(Expr::Single(lhs)),
+                _ => {}
+            }
+
+            let op = match iter.next() {
+                Some(&Token::Plus) => Op::Plus,
+                Some(&Token::Minus) => Op::Minus,
+                Some(t) => return Err(ParseError::UnexpectedToken(*t)),
+                None => return Err(ParseError::UnexpectedEol),
+            };
+            let rhs = match parse_operand(iter) {
+                Ok(ret) => ret,
+                Err(e) => return Err(e),
+            };
+
+            Ok(Expr::Compound {
+                op: op,
+                left: lhs,
+                right: rhs,
+            })
+        }
+
+        /// parse one operand
+        /// ```
+        /// operand := ⟨num⟩ | "(" ⟨expr⟩ ")"
+        /// ```
+        fn parse_operand<'a>(iter: &mut Iter<'a, Token>) -> Result<Operand, ParseError> {
+            let token = match iter.next() {
+                None => return Err(ParseError::UnexpectedEol),
+                Some(ret) => *ret,
+            };
+
+            match token {
+                Token::Number(n) => return Ok(Operand::Num(n)),
+                Token::OpenParen => match parse_expr(iter) {
+                    Err(e) => return Err(e),
+                    Ok(ret) => match iter.next() {
+                        Some(&Token::CloseParen) => Ok(Operand::Exp(Box::new(ret))),
+                        _ => Err(ParseError::MissingParen),
+                    },
+                },
+                _ => return Err(ParseError::UnexpectedToken(token)),
+            }
+        }
+
+        // create iterator and start recursive parsing
+        let iter = &mut input.iter();
+        let ret = parse_expr(iter);
+
+        match (ret, iter.next()) {
+            (Ok(ret), None) => Ok(ret), // result ok and nothing left: check
+            (Ok(_), _) => Err(ParseError::UnnecessaryToken), // result ok, but token left
+            (Err(e), _) => Err(e),      // some error during expression parsing
+        }
+    }
+}
 
 /// Reads a string from the user (with a nice prompt).
 fn read_string() -> String {
@@ -32,4 +264,24 @@ fn read_string() -> String {
     buffer.truncate(new_len);
 
     buffer
+}
+
+#[test]
+fn test_expr() {
+    let r = Expr::Compound {
+        op: Op::Plus,
+        left: Operand::Num(4),
+        right: Operand::Num(5),
+    };
+    let l = Expr::Compound {
+        op: Op::Plus,
+        left: Operand::Num(6),
+        right: Operand::Num(5),
+    };
+    let t2 = Expr::Compound {
+        op: Op::Minus,
+        left: Operand::Exp(Box::new(l)),
+        right: Operand::Exp(Box::new(r)),
+    };
+    assert_eq!(t2.evaluate(), 2);
 }
